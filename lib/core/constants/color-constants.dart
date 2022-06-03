3668caf9f67fb69f69cfc13b3a6524ec80ad5f3b
Furkan KAN <furkankan.188@gmail.com>
2022-06-03 12:11:51 +0300

    Update color-constants.dart

--- lib/core/constants/color-constants.dart
+++ lib/core/constants/color-constants.dart
@@ -8,9 +8,7 @@ class AppColors {
   static const Color generalGreyColor = Color(0xFF8C8CB1);
   static const Color appGrey = Color(0xFF4D4E74);
   static const Color softGreyLight = Color(0xFFF6F6FB);
-  static const Color appBlue = Color(0xFF517EFF);
   static const Color appWhiteGrey = Color(0xFFE5E5F0);
   static const Color appGreen = Color(0xFF28CB69);
-  static const Color darkOrange = Color(0xFFDD9F14);
   static const Color appRed = Color(0xFFEB1919);
 }
